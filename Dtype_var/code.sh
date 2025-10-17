379e33b1d3a49bb7fd22d1e97f3504939ed0d319
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-10-17 11:24:38 +0530

    Add files via upload

--- Dtype_var/code.sh
+++ Dtype_var/code.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-name="Hello World"
-phone=432534534
-# echo $name
-# echo "Name: $name, Phone: $phone"
-echo "Name: $name"
-echo "Phone: $phone"
+# name="Hello World"
+# phone=432534534
+# # echo $name
+# # echo "Name: $name, Phone: $phone"
+# echo "Name: $name"
+# echo "Phone: $phone"
+
+
+# Addition, Sub, mul ---------
+# num1=10
+# num2=20
+
+# sum1=$((num1+num2))
+# sum2=$((num1-num2))
+# sum3=$((num1/num2))
+# sum4=$((num1*num2))
+# sum5=$((num1%num2))
+
+# echo value of num1 and num2 is $sum2 
+# echo value of num1 and num2 is $sum1 
+# echo value of num1 and num2 is $sum4 
+# echo value of num1 and num2 is $sum3
+# echo value of num1 and num2 is $sum5
+
+
+# Directry Display---------
+
+# code1=$(pwd)
+# echo "PWD: " $code1
+
+# code2=$(ls)
+# echo "LS: " $code2
+
+# code3=$(cd)
+# echo "CD: " $code3
