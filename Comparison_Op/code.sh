b8c017dd9c6133eebda6173a84b1a8b8deb02080
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-10-17 11:52:23 +0530

    Add files via upload

---
+++ Comparison_Op/code.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# if [5 < 10];then
+# -lt = less than
+# -gt = greater than
+# -ge = greater than and equals to 
+# -le = less than and equals to 
+
+
+# if [ 5 -ge 10 ];then 
+# if [ 5 -le 5 ];then 
+if [ 6  -le 5 ];then 
+    # echo "5 is less than 10";
+    # echo "5 is greater and equals";
+    echo "true";
+else
+    # echo "5 is not less than 10";
+    echo "false";
+
+fi #closed if condition
